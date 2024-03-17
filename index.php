af0365f4d336495d15d3ec861ba0eb69010024bb
Arniel E. Casile <arniel.casile@personiv.com>
2024-03-17 12:50:29 +0800

    Added htaccess and index in root folder

---
+++ index.php
@@ -0,0 +1,23 @@
+<!--Added Nikko Zabala visit me at www.nikkozabala.me -->
+
+<?php
+
+$loc = '/online-dashboard-uat/public/login';
+header("Location: $loc");
+die(0);
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="robots" content="noindex">
+	<title>CS - Online Dashboard</title>
+</head>
+<body>
+<h1>Hello World</h1>
+
+<a href="/online-dashboard-uat/public">Online Dashboard</a>
+</body>
+</html>
